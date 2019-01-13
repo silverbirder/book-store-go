add40e8e6a4e6a467bd68967f76d590b15cecc4a
shibamoto <silverbirder@gmail.com>
2019-01-13 12:21:38 +0900

    add

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM golang:1.8
+LABEL name "book-store-go"
+WORKDIR /go/src/app
+COPY . .
+RUN go-wrapper download
+RUN go-wrapper install
+EXPOSE 3000
+ENTRYPOINT ["go-wrapper", "run"]
---
+++ index.go
@@ -0,0 +1,14 @@
+// main.go
+package main
+
+import (
+	"github.com/andoshin11/go-web-app/src/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", controller.IndexGET)
+	router.Run(":3000")
+}
